99ef05cebd277d175f4f2cd275ce0556793338b4
Nathan Wilce <me@nwilce.co.uk>
2021-07-18 21:10:18 +0000

    Implemented Balance Command

---
+++ app/Commands/GetBalance.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Commands;
+
+use App\Models\Account;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class GetBalance extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'atm:balance
+                            {account-number : The number of the account}
+                            {account-pin : The accounts pin number}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Gets account balance';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $account_number = $this->argument('account-number');
+        $account_pin = $this->argument('account-pin');
+
+        $account = Account::find($account_number);
+
+        if ($account && $account->pin == $account_pin) {
+            $this->info('Balance: ' . $account->balance);
+            $this->info('Available: ' . ($account->balance + $account->overdraft_maximum));
+        } else {
+            $this->error('Invalid account number or pin');
+        }
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
